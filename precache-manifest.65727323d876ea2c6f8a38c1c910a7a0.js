e9fc00d17ffe7d1d4429896f5b2e0910e8b4d384
Kushal Manek <kushalmanek2000@gmail.com>
2021-05-21 05:10:28 +0530

    Updates

---
+++ precache-manifest.65727323d876ea2c6f8a38c1c910a7a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69eeb95100c6f1d41bd6",
+    "url": "/monst/static/css/main.cb66427b.chunk.css"
+  },
+  {
+    "revision": "69eeb95100c6f1d41bd6",
+    "url": "/monst/static/js/main.da80df55.chunk.js"
+  },
+  {
+    "revision": "24fde33b3f596fe3d3df",
+    "url": "/monst/static/js/runtime~main.863bb7a1.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/monst/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "7db361069cc52ba48096",
+    "url": "/monst/static/js/3.63a8b96a.chunk.js"
+  },
+  {
+    "revision": "01751161dbd9e617ea37289e77e87758",
+    "url": "/monst/index.html"
+  }
+];
